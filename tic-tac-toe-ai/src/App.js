b58f751b99809060a1a02aa7cd2319e46e762561
disha-baghel <bagheldisha708@gmail.com>
2024-01-16 19:59:15 +0530

    refactored

--- tic-tac-toe-ai/src/App.js
+++ tic-tac-toe-ai/src/App.js
@@ -23,31 +23,53 @@
 // }
 
 // export default App;
-import React from 'react';
-import './output.css';
+import React from "react";
+import {useState} from 'react';
+import "./output.css";
 
-function Square() {
+function Square({value}) {
+
+
+  function handleClick() {
+    console.log("click");
+  }
+
   return (
     <>
-    <div class="place-content-center">
-    <div class="board-row">
-    <button class = "square mx-auto bg-slate-200 w-24 h-24">X</button>
-    <button class = "square mx-auto bg-slate-200 w-24 h-24 border-x-2 border-black">X</button>
-    <button class = "square mx-auto bg-slate-200 w-24 h-24 shadow-lg">X</button>
-    </div>
-    <div class="board-row">
-    <button class = "square mx-auto bg-slate-200 w-24 h-24 border-y-2 border-black shadow-lg">X</button>
-    <button class = "square mx-auto bg-slate-200 w-24 h-24 border-2 border-black shadow-lg">X</button>
-    <button class = "square mx-auto bg-slate-200 w-24 h-24 border-y-2 border-black shadow-lg">X</button>
-    </div>
-    <div class="board-row">
-    <button class = "square mx-auto bg-slate-200 w-24 h-24 shadow-lg">X</button>
-    <button class = "square mx-auto bg-slate-200 w-24 h-24 border-x-2 border-black shadow-lg">X</button>
-    <button class = "square mx-auto bg-slate-200 w-24 h-24 shadow-lg">X</button>
-    </div>
-    </div>
+      <div class="place-content-center">
+        <div class="board-row">
+          <button class="square mx-auto bg-slate-200 w-24 h-24" onClick={handleClick}>{value}</button>
+          <button class="square mx-auto bg-slate-200 w-24 h-24 border-x-2 border-black" onClick={handleClick}>
+            {value}
+          </button>
+          <button class="square mx-auto bg-slate-200 w-24 h-24" onClick={handleClick}>
+            {value}
+          </button>
+        </div>
+        <div class="board-row">
+          <button class="square mx-auto bg-slate-200 w-24 h-24 border-y-2 border-black" onClick={handleClick}>
+            {value}
+          </button>
+          <button class="square mx-auto bg-slate-200 w-24 h-24 border-2 border-black" onClick={handleClick}>
+            {value}
+          </button>
+          <button class="square mx-auto bg-slate-200 w-24 h-24 border-y-2 border-black" onClick={handleClick}>
+            {value}
+          </button>
+        </div>
+        <div class="board-row">
+          <button class="square mx-auto bg-slate-200 w-24 h-24 shadow-lg" onClick={handleClick}>
+            {value}
+          </button>
+          <button class="square mx-auto bg-slate-200 w-24 h-24 border-x-2 border-black shadow-lg" onClick={handleClick}>
+            {value}
+          </button>
+          <button class="square mx-auto bg-slate-200 w-24 h-24 shadow-lg" onClick={handleClick}>
+            {value}
+          </button>
+        </div>
+      </div>
     </>
-    
   );
 }
 export default Square;
