44b731e7ccb41943f858b2edb4901695c66d3571
disha-baghel <bagheldisha708@gmail.com>
2024-01-16 17:29:29 +0530

    created the board for tictactoe :

--- tic-tac-toe-ai/src/App.js
+++ tic-tac-toe-ai/src/App.js
@@ -23,3 +23,31 @@
 // }
 
 // export default App;
+import React from 'react';
+import './output.css';
+
+function Square() {
+  return (
+    <>
+    <div class="place-content-center">
+    <div class="board-row">
+    <button class = "square mx-auto bg-slate-200 w-24 h-24">X</button>
+    <button class = "square mx-auto bg-slate-200 w-24 h-24 border-x-2 border-black">X</button>
+    <button class = "square mx-auto bg-slate-200 w-24 h-24 shadow-lg">X</button>
+    </div>
+    <div class="board-row">
+    <button class = "square mx-auto bg-slate-200 w-24 h-24 border-y-2 border-black shadow-lg">X</button>
+    <button class = "square mx-auto bg-slate-200 w-24 h-24 border-2 border-black shadow-lg">X</button>
+    <button class = "square mx-auto bg-slate-200 w-24 h-24 border-y-2 border-black shadow-lg">X</button>
+    </div>
+    <div class="board-row">
+    <button class = "square mx-auto bg-slate-200 w-24 h-24 shadow-lg">X</button>
+    <button class = "square mx-auto bg-slate-200 w-24 h-24 border-x-2 border-black shadow-lg">X</button>
+    <button class = "square mx-auto bg-slate-200 w-24 h-24 shadow-lg">X</button>
+    </div>
+    </div>
+    </>
+    
+  );
+}
+export default Square;
