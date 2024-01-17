bf242d6367be45f033f40ce774ca0db8837352b3
disha-baghel <bagheldisha708@gmail.com>
2024-01-17 20:24:14 +0530

    some files deleted

--- tic-tac-toe-ai/src/App.js
+++ tic-tac-toe-ai/src/App.js
@@ -1,41 +1,17 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       {/* <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header> */}
-//     </div>
-//   );
-// }
-
-// export default App;
 import React from "react";
 import {useState} from 'react';
-import "./App.css";
 
 function Square() {
   const [value, setValue] = useState("1");
 
   function handleClick() {
     console.log("click");
+    setValue("2");
+    
   }
 
   return (
-    <button class="square bg-slate-200 w-24 h-24 hover:bg-slate-300" onClick={handleClick}>
+    <button className="square bg-slate-200 w-24 h-24 hover:bg-slate-300" onClick={handleClick}>
       {value}
     </button>
   );
@@ -45,19 +21,19 @@ function Square() {
 function Board() {
   return (
     <>
-      <div class="place-content-center">
-        
-        <div class="board-row">
+      <div className="flex justify-center items-center flex-col h-screen">
+        <h1 className="text-3xl">Tic Tac Toe</h1>
+        <div className="board-row">
           <Square />
           <Square />
           <Square />
         </div>
-        <div class="board-row">
+        <div className="board-row">
           <Square />
           <Square />
           <Square />
         </div>
-        <div class="board-row">
+        <div className="board-row">
           <Square />
           <Square />
           <Square />
