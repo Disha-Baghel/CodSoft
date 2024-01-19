1251b61a65b1d1a02d5a7a5343b95825cdc6320f
disha-baghel <bagheldisha708@gmail.com>
2024-01-19 20:39:33 +0530

    some changes

--- tic-tac-toe-ai/src/components/Block.js
+++ tic-tac-toe-ai/src/components/Block.js
@@ -1,19 +1,9 @@
 import React from "react";
 
-const Block = ({value, onclick}) => {
-    const style = value === 'X' ? "box x" : "box o";
-    // const [value, setValue] = useState(".");
-  
-  
-  
-    function handleClick() {
-      console.log("click");
-      
-      
-    }
+const Block = ({value, onClick}) => {
   
     return (
-      <button className="{style} square bg-slate-200 w-24 h-24 hover:bg-slate-300 border-2 border-gray-400" onClick={onclick}>
+      <button className="square bg-slate-200 w-24 h-24 hover:bg-slate-300 border-2 border-gray-400 text-5xl font-bold" onClick={onClick}>
         {value}
       </button>
     );
--- tic-tac-toe-ai/src/components/Board.js
+++ tic-tac-toe-ai/src/components/Board.js
@@ -8,7 +8,7 @@ const Board = ({board, onClick}) => {
   return (
     <div className="grid justify-center items-center grid-cols-3 border-4 border-gray-500 shadow-2xl">
       {board.map((value, index) => {
-        return <Block value={value} onClick={() => onClick(index)}/>
+        return <Block value={value} onClick={() => value === null && onClick(index)}/>
       })}
     </div>
   );
