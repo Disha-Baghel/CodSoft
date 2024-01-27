6dddaac084cd6798240aa9183bbc6314141897c5
disha-baghel <bagheldisha708@gmail.com>
2024-01-27 16:51:31 +0530

    Facial Detection using Haar Cascades

---
+++ Facial Detection/main.py
@@ -0,0 +1,21 @@
+import cv2
+
+cascade_path = '/usr/share/opencv4/haarcascades/haarcascade_frontalface_default.xml'
+
+face_cascade = cv2.CascadeClassifier(cascade_path)
+
+video = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = video.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 2)
+    cv2.imshow('Video', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+video.release()
+cv2.destroyAllWindows()
+
